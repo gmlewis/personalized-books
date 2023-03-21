7a0afef722cc6471b3f6fca6f98ae4f1876a5020
Parker77 <20973702+Parker77@users.noreply.github.com>
2023-03-21 12:21:52 -0400

    Update text

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="paragraphs">
     <p>Are you searching for the perfect personalized gift for someone who enjoys puzzles? You found it!</p>
-    <p>What better way to surprise your friend, coworker, or loved one? Show them that you care by sending them their own
-      personalized puzzle book today!</p>
-    <p>Search for their name to see what books are available:</p>
+    <p>What better way to surprise your friends, coworkers, or loved ones? Show them that you care by sending them their
+      own personalized puzzle books today!</p>
+    <p>Search for a name to see what books are available:</p>
   </div>
   <SearchBooks />
 </template>
